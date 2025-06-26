0564bc531108499bf03779cae62a92f876e794bc
Lucas <lucas@somfic.dev>
2025-06-26 18:18:51 +0200

    basic hyprland config

--- home.nix
+++ home.nix
@@ -59,15 +59,21 @@
     kitty = { enable = true; };
   };
 
-  wayland.windowManager.hyprland.settings = {
-    "$mod" = "SUPER";
-    cursor = {
-      inactive_timeout = 0;
-      no_hardware_cursors = true;
-      hide_on_key_press = true;
-    };
-    bind = [ "$mod, F, exec, firefox" ", Print, exec, grimblast copy area" ]
-      ++ (
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$mod" = "SUPER";
+      cursor = {
+        inactive_timeout = 0;
+        no_hardware_cursors = true;
+        hide_on_key_press = false;
+      };
+      bind = [
+        "$mod, F, exec, firefox"
+        "$mod, Q, exec, kitty"
+        "$mod, C, killactive"
+        ", Print, exec, grimblast copy area"
+      ] ++ (
         # workspaces
         # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
         builtins.concatLists (builtins.genList (i:
@@ -76,6 +82,7 @@
             "$mod, code:1${toString i}, workspace, ${toString ws}"
             "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
           ]) 9));
+    };
   };
 
   home.username = "lucas";
