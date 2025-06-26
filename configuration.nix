849f0d0b1ae769324584b647bdc04d9a1a6eb5ae
Lucas <lucas@somfic.dev>
2025-06-26 23:16:02 +0200

    refactor bootloader configuration for improved EFI support

--- configuration.nix
+++ configuration.nix
@@ -29,8 +29,17 @@
   };
 
   # bootloader
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader = {
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
+    };
+    grub = {
+      efiSupport = true;
+      #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+      device = "nodev";
+    };
+  };
 
   # hostname  
   networking.hostName = "nixos";
